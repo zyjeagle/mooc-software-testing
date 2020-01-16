4b28914c194ac024576eefd0f3552f961c2f2783
menethil <zyjeagle@outlook.com>
2020-01-15 19:13:35 -0800

    @BeforeEach

--- src/test/java/tudelft/gettingstarted/GettingStartedTest.java
+++ src/test/java/tudelft/gettingstarted/GettingStartedTest.java
@@ -1,13 +1,22 @@
 package tudelft.gettingstarted;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class GettingStartedTest {
 
+    GettingStarted gs;
+
+    @BeforeEach
+    public void setUp(){
+        gs = new GettingStarted();
+    }
+
     @Test
     public void addFiveTo20() {
-        int result = new GettingStarted().addFive(20);
+        int result = gs.addFive(20);
         Assertions.assertEquals(25,result);
     }
 
@@ -15,19 +24,19 @@ public class GettingStartedTest {
 
     @Test
     public void addFiveToZero() {
-        int result = new GettingStarted().addFive(0);
+        int result = gs.addFive(0);
         Assertions.assertEquals(5, result);
     }
 
     @Test
     public void addFiveToMinus20() {
-        int result = new GettingStarted().addFive(-20);
+        int result = gs.addFive(-20);
         Assertions.assertEquals(-15,result);
     }
 
     @Test
     public void addFiveToMinus3(){
-        int result = new GettingStarted().addFive(-3);
+        int result = gs.addFive(-3);
         Assertions.assertTrue(result==2);
     }
 }
